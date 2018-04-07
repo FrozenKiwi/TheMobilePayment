0386631e0800890a0561343fca74bbe9060a3e59
Stephen Taylor <stephen.taylor.dev@gmail.com>
2018-04-07 12:22:04 -0400

    pass-through server functional, although untested for actual purchase

--- PaymentServer/PaymentServer/Program.cs
+++ PaymentServer/PaymentServer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
 using Fleck;
+using NLog;
 using PCSC;
 using PCSC.Iso7816;
 
@@ -10,51 +12,72 @@ namespace PaymentServer
 {
     class Program
     {
-        private class Transaction
+        private class Transaction : IDisposable
         {
-            private IsoReader reader;
-            private ISCardContext context;
+            private IsoReader _reader;
+            private IsoReader Reader
+            {
+                get
+                {
+                    if (_reader == null)
+                    {
+                        var contextFactory = ContextFactory.Instance;
+                        Context = contextFactory.Establish(SCardScope.System);
+
+                        var readerNames = Context.GetReaders();
+                        if (NoReaderFound(readerNames))
+                        {
+                            Console.WriteLine("You need at least one reader in order to run this example.");
+                            Console.ReadKey();
+                            return null;
+                        }
+
+                        var name = ChooseReader(readerNames);
+                        if (name == null)
+                        {
+                            return null;
+                        }
+
+                        _reader = new IsoReader(
+                            context: Context,
+                            readerName: name,
+                            mode: SCardShareMode.Shared,
+                            protocol: SCardProtocol.Any,
+                            releaseContextOnDispose: false);
+
+                        logger.Info("Transaction Started: on {0} ", _reader.ReaderName);
+                    }
+                    return _reader;
+                }
+            }
+            private ISCardContext Context;
+
+            Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
             public Transaction()
             {
-                var contextFactory = ContextFactory.Instance;
-                context = contextFactory.Establish(SCardScope.System);
-
-                var readerNames = context.GetReaders();
-                if (NoReaderFound(readerNames))
-                {
-                    Console.WriteLine("You need at least one reader in order to run this example.");
-                    Console.ReadKey();
-                    return;
-                }
-
-                var name = ChooseReader(readerNames);
-                if (name == null)
-                {
-                    return;
-                }
-
-                reader = new IsoReader(
-                    context: context,
-                    readerName: name,
-                    mode: SCardShareMode.Exclusive,
-                    protocol: SCardProtocol.Any,
-                    releaseContextOnDispose: false);
             }
 
-            public byte[] SendCommand(CommandApdu apdu, String command)
+            public void Dispose()
             {
-                Console.WriteLine("Sending First {0}: {1}",
-                    command, BitConverter.ToString(apdu.ToArray()));
+                if (_reader != null)
+                {
+                    logger.Trace("Disposing Reader");
+                    _reader?.Dispose();
+                }
+            }
 
-                var response = reader.Transmit(apdu);
+            public Response SendCommand(CommandApdu apdu, String command)
+            {
+                logger.Trace("Sending Command {0}: {1}", command, BitConverter.ToString(apdu.ToArray()));
 
-                Console.WriteLine("SW1 SW2 = {0:X2} {1:X2}",
-                    response.SW1, response.SW2);
+                var response = Reader.Transmit(apdu);
+
+                logger.Trace("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2);
 
                 if (!response.HasData)
                 {
-                    Console.WriteLine("No data. (Card does not understand \"{0}\")", command);
+                    logger.Trace("No data. (Card does not understand \"{0}\")", command);
                     return null;
                 }
                 else
@@ -62,42 +85,49 @@ namespace PaymentServer
                     var resp = response.GetData();
                     var chars = System.Text.Encoding.UTF8.GetString(resp);
 
-                    Console.WriteLine("Response: {0}", BitConverter.ToString(resp), chars);
+                    string asString = "";
                     foreach (char ch in chars)
                     {
                         // Skip that annoying beep
                         if (ch != 0x07)
-                            Console.Write(ch);
+                            asString += ch;
                     }
-                    Console.Write("\n");
-                    return resp;
+                    logger.Trace("Response: \n  {0}\n  {1}", BitConverter.ToString(resp), asString);
                 }
+                return response;
             }
 
-            public byte[] SendCommand(byte[] data, String origin)
+            public Response SendCommand(byte[] data, String origin)
             {
                 // We could simply pass the data directly through by 
                 // using the lower-level API, but it appears that the
                 // reader/apdu combo provided by the PCSC library does
                 // a bit of additional work on reading to ensure we
                 // interface with the card correctly, so we route through it
-                CommandApdu apdu = new CommandApdu(IsoCase.Case4Short, reader.ActiveProtocol);
+                bool hasData = data.Length > 5;
+                IsoCase commandCase = hasData ? IsoCase.Case4Short : IsoCase.Case2Short;
+
+                CommandApdu apdu = new CommandApdu(commandCase, Reader.ActiveProtocol);
                 apdu.CLA = data[0];
                 apdu.Instruction = (InstructionCode)data[1];
                 apdu.P1 = data[2];
                 apdu.P2 = data[3];
 
-                if (data.Length > 4)
+                if (hasData)
                 {
-                    Buffer.BlockCopy(data, 5, apdu.Data, 0, data.Length - 4);
+                    // TODO!!! The skipped byte is the Lc byte.  This field
+                    // may actually be longer than 255 though, in which case
+                    // we may need multiple bytes
+                    byte[] buffer = new byte[data.Length - 5];
+                    Buffer.BlockCopy(data, 5, buffer, 0, data.Length - 5);
+                    apdu.Data = buffer;
                 }
 
                 // For validation, convert back to byte array, and check equality
                 var newArray = apdu.ToArray();
                 if (!newArray.SequenceEqual(data))
                 {
-                    String errormsg = String.Format("Reconstructing APDU Failed! \n  Orig={0}\n  Recon={1}", data, newArray);
-                    FleckLog.Error(errormsg);
+                    logger.Error("Reconstructing APDU Failed! \n  Orig={0}\n  Recon={1}", data, newArray);
                     // TODO: return some sort of error message
                 }
                 return SendCommand(apdu, origin);
@@ -119,43 +149,60 @@ namespace PaymentServer
 
         static void Main(string[] args)
         {
-            FleckLog.Level = LogLevel.Debug;
+            //FleckLog.Level = Fleck.LogLevel.Debug;
+            Logger logger = NLog.LogManager.GetLogger("Main");
+
             var allSockets = new List<IWebSocketConnection>();
             var server = new WebSocketServer("ws://0.0.0.0:1379");
             // Each socket has a transaction attached
-            var socketTransactions = new Dictionary<IWebSocketConnection, Transaction>();
+            var socketTransactions = new ConcurrentDictionary<IWebSocketConnection, Transaction>();
 
             server.Start(socket =>
             {
                 socket.OnOpen = () =>
                 {
-                    FleckLog.Debug("Open!");
-                    Console.WriteLine("Open!");
+                    logger.Trace("Open!");
                     allSockets.Add(socket);
-
-                    socketTransactions.Add(socket, new Transaction());
                 };
                 socket.OnClose = () =>
                 {
-                    FleckLog.Debug("Close!");
-                    Console.WriteLine("Close!");
+                    logger.Trace("Close!");
                     allSockets.Remove(socket);
 
-                    socketTransactions.Remove(socket);
+                    Transaction finishedTransaction;
+                    socketTransactions.TryRemove(socket, out finishedTransaction);
+                    finishedTransaction?.Dispose();
+
                 };
                 socket.OnMessage = message =>
                 {
-
-                    Console.WriteLine(message);
-                    
+                    logger.Info("Message: {0}", message);
                 };
                 socket.OnBinary = data =>
                 {
-                    Transaction currentTransaction;
-                    if (socketTransactions.TryGetValue(socket, out currentTransaction))
+                    logger.Trace("Binary Received");
+
+                    Transaction currentTransaction = socketTransactions.GetOrAdd(socket, new Transaction());
+                    if (currentTransaction != null)
                     {
-                        var resp = currentTransaction.SendCommand(data, socket.ConnectionInfo.Origin);
-                        socket.Send(resp);
+                        var response = currentTransaction.SendCommand(data, socket.ConnectionInfo.Origin);
+                        // return a reconstructed buffer from all
+                        // contained apdu's in this response
+                        if (response != null)
+                        {
+                            byte[] swbytes = { response.SW1, response.SW2 };
+                            var bytes = response.GetData().Concat(swbytes).ToArray();
+                            socket.Send(bytes);
+                        }
+                        else
+                        {
+                            byte[] bytes = System.Text.Encoding.ASCII.GetBytes("ERROR");
+                            socket.Send(bytes);
+                        }
+                    }
+                    else
+                    {
+                        logger.Error("No Transaction Registered!");
                     }
                 };
             });
